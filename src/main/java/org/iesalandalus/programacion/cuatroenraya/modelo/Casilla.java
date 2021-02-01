393ff3e8e4e1d02d67844b65d0dfb7a2dece5860
admin <admin@DESKTOP-47JDCQM>
2021-02-01 14:44:34 +0100

    creacion de los metodos get y set ,constructor por defecto que inicialmente pondrá el atributo ficha a null, el método estaOcupada que devolverá true o falsey el metodo tostring

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Casilla.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Casilla.java
@@ -1,6 +1,39 @@
 package org.iesalandalus.programacion.cuatroenraya.modelo;
 
+import javax.naming.OperationNotSupportedException;
+
 public class Casilla {
 	Ficha ficha;
-	
-}
+	public Ficha getFicha() {
+		return ficha;
+	}
+	public void setFicha(Ficha ficha) throws OperationNotSupportedException {
+		 if(estaOcupada() == true) {
+				throw new OperationNotSupportedException("ERROR: Ya contengo una ficha.");
+			}
+		 else
+			if(ficha == null) {
+				throw new NullPointerException("ERROR: No se puede poner una ficha nula.");
+			} else if(ficha == Ficha.AZUL) {
+				this.ficha = Ficha.AZUL;
+			} else if(ficha == Ficha.VERDE) {
+				this.ficha = Ficha.VERDE;
+			}
+	}
+	public boolean estaOcupada() {
+		return this.ficha != null;
+	}
+	@Override
+	public  String toString() {
+		if (ficha != null) {
+			if (ficha.name().equals("AZUL"))
+				{
+				return "A";
+				}
+			else if (ficha.name().equals("VERDE"))
+				{
+				return "V";
+				}}
+
+		return " "; 
+	}}
