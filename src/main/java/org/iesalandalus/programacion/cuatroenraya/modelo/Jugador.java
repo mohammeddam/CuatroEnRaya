ba8bdcce4013fc14bbcff724edcc486b9f926028
admin <admin@DESKTOP-47JDCQM>
2021-02-01 15:03:21 +0100

    creacion de los metodos get y set.

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Jugador.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Jugador.java
@@ -1,5 +1,31 @@
 package org.iesalandalus.programacion.cuatroenraya.modelo;
 
 public class Jugador {
+	private String nombre;
+	private Ficha colorFichas;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	private void setNombre(String nombre) {
+		if (nombre == null) {
+			throw new NullPointerException("ERROR: El nombre no puede ser nulo.");
+		}
+		if (nombre.trim().length() == 0) {
+			throw new IllegalArgumentException("ERROR: El nombre no puede estar vacío.");
+		}
+		this.nombre = nombre;
+	}
+	public Ficha getColorFichas() {
+		return colorFichas;
+	}
+	private void setColorFichas(Ficha colorFichas) {
+		if (colorFichas == null) {
 
+			throw new NullPointerException("ERROR: El color de las fichas no puede ser nulo.");
+		}
+		
+		this.colorFichas = colorFichas;
+	}
+	
 }
